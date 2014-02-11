37bcea00becac4b6cfe25ac310c5ae175eab38ce
Logan Hasson <logan.hasson@gmail.com>
2014-02-11 11:29:46 -0500

    Add more complex example to count_sentences_spec.rb

--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -9,4 +9,10 @@ describe String, "#count_sentences" do
   it "returns zero if there are no sentences in a string" do
     expect("".count_sentences).to eq(0)
   end
+
+  it "returns the number of sentences in a complex string" do
+    complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+    expect(complex_string.count_sentences).to eq(5)
+  end
+
 end
