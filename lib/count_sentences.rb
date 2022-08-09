6bb427c23a8b94c452e72a2674151eb44311929c
Christopher Marzulli <chriszulli15@gmail.com>
2022-08-09 11:10:05 -0400

    commit tree c4919624366b43c3c8dc7be7a1c43b9fb985ee08

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    no_empty_array = array.reject { |a| a.empty?}
+    no_empty_array.length
   end
 end
